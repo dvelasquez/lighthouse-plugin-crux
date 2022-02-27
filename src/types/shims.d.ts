c683ef49a75627d3842b50c7b2cafd64ce7ef7f4
Danilo Velasquez <null>
2022-02-27 08:18:15 +0100

    updated lighthouse, devtools, typescript and fixed type issues

--- src/types/shims.d.ts
+++ src/types/shims.d.ts
@@ -1,23 +1,3 @@
-/// <reference path="../../node_modules/lighthouse/types/artifacts.d.ts" />
-/// <reference path="../../node_modules/lighthouse/types/audit.d.ts" />
-/// <reference path="../../node_modules/lighthouse/types/audit-details.d.ts" />
-/// <reference path="../../node_modules/lighthouse/types/budget.d.ts" />
-/// <reference path="../../node_modules/lighthouse/types/config.d.ts" />
-/// <reference path="../../node_modules/lighthouse/types/externs.d.ts" />
-/// <reference path="../../node_modules/lighthouse/types/gatherer.d.ts" />
-/// <reference path="../../node_modules/lighthouse/types/html-renderer.d.ts" />
-/// <reference path="../../node_modules/lighthouse/types/i18n.d.ts" />
-/// <reference path="../../node_modules/lighthouse/types/jest.d.ts" />
-/// <reference path="../../node_modules/lighthouse/types/jsonlint-mod.d.ts" />
-/// <reference path="../../node_modules/lighthouse/types/lhr.d.ts" />
-/// <reference path="../../node_modules/lighthouse/types/node.d.ts" />
-/// <reference path="../../node_modules/lighthouse/types/protocol.d.ts" />
-/// <reference path="../../node_modules/devtools-protocol/types/protocol.d.ts" />
-/// <reference path="../../node_modules/devtools-protocol/types/protocol-mapping.d.ts" />
-/// <reference path="../../node_modules/devtools-protocol/types/protocol-proxy-api.d.ts" />
-/// <reference path="../../node_modules/lighthouse/types/smokehouse.d.ts" />
-/// <reference path="../../node_modules/lighthouse/types/structured-data.d.ts" />
-// @ts-ignore
-declare module 'lighthouse/lighthouse-core/computed/network-records';
+/// <reference path="../../node_modules/lighthouse/types/global-lh.d.ts" />
 
 export {};
